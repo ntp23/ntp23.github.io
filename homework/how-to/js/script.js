fe59b7ac45355b3c89651301897c000b64e95a88
Nicole Pellicone <ntp23@miami.edu>
2018-11-01 15:45:08 -0400

    Version 15/3

---
+++ homework/how-to/js/script.js
@@ -0,0 +1,52 @@
+var totalsteps=5;
+
+var currentstep=0;
+
+var currentslide;
+
+$(function(){
+  $('totalsteps').html(totalsteps);
+  $('.steps li, .end, #prev').hide()
+});
+$('.begin').on('click', function(){
+  $('.begin').hide();
+  currentstep = 1;
+  currentslide=$('#step1');
+  $(currentslide).fadeIn(1000);
+  $('.controls').fadeIn(1000);
+  $('h2').html('Step '+ currentstep);
+});
+$('#next').on('click', function(){
+  if(currentstep != totalsteps){
+  $(currentslide).hide();
+  currentstep++;
+  currentslide=$('#step' + currentstep);
+  $(currentslide).fadeIn(1000);
+  $('h2').html('Step '+ currentstep);
+}
+  if(currentstep == totalsteps){
+  $('#next').hide();
+  }
+  if(currentstep != 1){
+    $('#prev').show();
+  }
+});
+$('#prev').on('click', function(){
+
+  $(currentslide).hide();
+  currentstep--;
+  currentslide=$('#step' + currentstep);
+  $(currentslide).fadeIn(1000);
+  $('h2').html('Step '+ currentstep);
+  if(currentstep != totalsteps){
+  $('#next').show();
+  }
+  if(currentstep == 1){
+    $('#prev').hide();
+  }
+});
+$('.end').on(click, function(){
+  $(currentslide).hide();
+  currentslide=
+
+});
